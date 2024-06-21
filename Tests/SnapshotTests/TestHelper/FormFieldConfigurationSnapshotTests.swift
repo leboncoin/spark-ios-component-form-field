0a6c079791b0cae4c4fb69df58cbc11492351128
Robin Lemaire <robin.lemaire@adevinta.com>
2024-06-21 16:09:37 +0200

    Migrate from spark-ios

---
+++ Tests/SnapshotTests/TestHelper/FormFieldConfigurationSnapshotTests.swift
@@ -0,0 +1,36 @@
+//
+//  FormFieldConfigurationSnapshotTests.swift
+//  SparkFormFieldSnapshotTests
+//
+//  Created by alican.aycil on 08.04.24.
+//  Copyright © 2024 Adevinta. All rights reserved.
+//
+
+import UIKit
+@testable import SparkFormField
+@_spi(SI_SPI) import SparkCommonSnapshotTesting
+
+struct FormFieldConfigurationSnapshotTests {
+
+    // MARK: - Properties
+
+    let scenario: FormFieldScenarioSnapshotTests
+    let feedbackState: FormFieldFeedbackState
+    let label: String?
+    let helperMessage: String?
+    let isRequired: Bool
+    let isEnabled: Bool
+    let modes: [ComponentSnapshotTestMode]
+    let sizes: [UIContentSizeCategory]
+
+    // MARK: - Getter
+
+    func testName() -> String {
+        return [
+            "\(self.scenario.rawValue)",
+            "\(self.feedbackState)",
+            "IsRequired:\(self.isRequired)",
+            "IsEnabled:\(self.isEnabled)"
+        ].joined(separator: "-")
+    }
+}
---
+++ Tests/UnitTests/UseCase/FormFieldColorsUseCaseTests.swift
@@ -0,0 +1,57 @@
+//
+//  FormFieldColorsUseCaseTests.swift
+//  SparkFormField
+//
+//  Created by alican.aycil on 26.03.24.
+//  Copyright © 2024 Adevinta. All rights reserved.
+//
+
+import XCTest
+import SwiftUI
+@testable import SparkFormField
+import SparkThemingTesting
+
+final class FormFieldColorsUseCaseTests: XCTestCase {
+
+    var sut: FormFieldColorsUseCase!
+    var theme: ThemeGeneratedMock!
+
+    override func setUp() {
+        super.setUp()
+
+        self.sut = .init()
+        self.theme = .mocked()
+    }
+
+    // MARK: - Tests
+
+    func test_execute_for_all_feedback_cases() {
+        let feedbacks = FormFieldFeedbackState.allCases
+
+        feedbacks.forEach {
+
+            let formfieldColors = sut.execute(from: theme, feedback: $0)
+
+            let expectedFormFieldColor: FormFieldColors
+
+            switch $0 {
+            case .default:
+                expectedFormFieldColor = FormFieldColors(
+                    title: theme.colors.base.onSurface,
+                    description: theme.colors.base.onSurface.opacity(theme.dims.dim1),
+                    asterisk: theme.colors.base.onSurface.opacity(theme.dims.dim1)
+                )
+            case .error:
+                expectedFormFieldColor = FormFieldColors(
+                    title: theme.colors.base.onSurface,
+                    description: theme.colors.feedback.error,
+                    asterisk: theme.colors.base.onSurface.opacity(theme.dims.dim1)
+                )
+            }
+
+            XCTAssertEqual(formfieldColors.title.uiColor, expectedFormFieldColor.title.uiColor)
+            XCTAssertEqual(formfieldColors.description.uiColor, expectedFormFieldColor.description.uiColor)
+            XCTAssertEqual(formfieldColors.asterisk.uiColor, expectedFormFieldColor.asterisk.uiColor)
+        }
+    }
+}
